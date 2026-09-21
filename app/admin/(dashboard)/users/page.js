52077b3b1e577b821aa7f772e83775bada699611
elongthan <elongthan5@gmail.com>
2026-09-21 12:01:35 +0800

    Add files via upload

--- app/admin/(dashboard)/users/page.js
+++ app/admin/(dashboard)/users/page.js
@@ -1,7 +1,9 @@
-import { listAdmins } from "@/actions/adminUsers";
+import { redirect } from "next/navigation";
+import { listAdmins, getMyRole } from "@/actions/adminUsers";
 import UsersClient from "@/components/admin/UsersClient";
 
 export default async function AdminUsersPage() {
+  if ((await getMyRole()) === "staff") redirect("/admin/products");
   const admins = await listAdmins();
   return <UsersClient admins={admins} />;
 }
